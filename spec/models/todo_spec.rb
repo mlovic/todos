e0ecc7ac2f1e8afb261dfec342bd71febad336d1
Marko Lovic <markolovic@hotmail.com>
2014-10-24 23:20:57 -0600

    Refactor completeions#create to move logic into the todo method

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Todo, "#completed?" do
 
     expect(todo).not_to be_completed
   end
+
+  describe Todo, "#complete?" do
+    it "update completed_at" do
+      todo = Todo.create!(completed_at: nil)
+
+      todo.complete!
+
+      todo.reload
+
+      expect(todo).to be_completed
+    end
+  end
 end
